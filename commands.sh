08eeedbcc20e8b440af31627fd1eeb05c5a6aae5
Roman Verenich <r_verenich@wargaming.net>
2018-08-15 17:57:43 +0300

    commands.sh for .gitconfig

--- commands.sh
+++ commands.sh
@@ -3,3 +3,4 @@
 ln -sf ~/.config/RVconfig/.bash_aliases ~/.bash_aliases
 ln -sf ~/.config/RVconfig/config_terminator ~/.config/terminator/config
 ln -sf ~/.config/RVconfig/.bashrc ~/.bashrc
+ln -sf ~/.config/RVconfig/.gitconfig ~/.gitconfig
